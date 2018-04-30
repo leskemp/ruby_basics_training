f0994b75e8e9e3724307830e3f532ecaff58fea7
Leslie Kemp <leslie.kemp@fluxfederation.com>
2018-05-01 10:19:59 +1200

    adding functionality around guessed letter

--- hangman-1.rb
+++ hangman-1.rb
@@ -21,12 +21,30 @@ while (wrong < 3) && (guessed != secret)
   puts "Hangman Secret Word"
   puts underscore_arr.join('')
   puts "Press 'Y' to see remaining letters: "
-  y = STDIN.gets.chomp #if input is 'Y' do next line
+  y = STDIN.gets.chomp
   puts y = "Y" || y = "y" ? alph_arr
+  puts "You have #{3-wrong} lives remaining"
 
-  # If wrong > 0 output how many lives they have left (3-wrong)
+  # input a guess, if the length is 1, search for matches, else compare directly
+  # to Secret
+  puts "Enter your guess (letter or full word): "
+  guessed = STDIN.gets.chomp
+  guess_len = guessed.length
 
-  # input a guess
+  if guessed_len == 1
+    #find all occurances of guess in secret
+    underscore_arr.each_with_index { |elm,idx|
+      # update the array to  if element == letter
+      alph_arr[idx] = guessed if elm == guessed
+      right = true
+    }
+
+    # if there's no match, increment wrong
+    wrong ++ if !right
+    # if this an option? --> !right ? wrong++
+  else
+
+  end
 
   # update underscore_arr if guess is correct.
   #Output underscore_arr
