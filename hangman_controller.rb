773f1c4d810269d3575248e6c1e5490e66270e4d
Leslie Kemp <leslie.kemp@fluxfederation.com>
2018-05-04 15:33:35 +1200

    refactoring hangman controller to be a class

--- hangman_controller.rb
+++ hangman_controller.rb
@@ -3,24 +3,29 @@ require_relative 'hangman'
 require_relative 'view'
 
 class HangmanGame
+  attr_reader :dictionary, :hangman, :view
 
-dictionary = ["bob", "leslie", "purple", "a"]
-hangman = Hangman.new(3, dictionary.sample)
-view = View.new
-
-view.show_welcome
-
-while hangman.running?
-  guess=""
-  view.show_status(hangman.show_hidden_word, hangman.show_available_letters,
-    hangman.lives)
-  until hangman.valid? guess
-    guess = view.prompt_for_guess
+  def initialize
+    dictionary = ["bob", "leslie", "purple", "a"]
+    hangman = Hangman.new(3, dictionary.sample)
+    view = View.new
   end
-  view.show_guess_outcome(hangman.make_a_move(guess))
-  view.wait_and_clear_screen
-end
 
-view.show_game_over(hangman.won?)
-view.show_secret(hangman.secret.join(''))
-view.wait_and_clear_screen
+  def play
+    view.show_welcome
+
+    while hangman.running?
+      guess=""
+      view.show_status(hangman.show_hidden_word, hangman.show_available_letters,
+        hangman.lives)
+      until hangman.valid? guess
+        guess = view.prompt_for_guess
+      end
+      view.show_guess_outcome(hangman.make_a_move(guess))
+      view.wait_and_clear_screen
+    end
+
+    view.show_game_over(hangman.won?)
+    view.show_secret(hangman.secret.join(''))
+    view.wait_and_clear_screen
+  end
